6e71aea118ccf803270e3a567414ff673fd2c422
Natalia392 <ntorrejonvalencia@gmail.com>
2024-02-25 20:06:55 -0300

    create routes for endpoints take the base for the endpoints and add the content for the routes from the routes.js in routes js, import express and use the router, and then export it

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config(); // imports content of .env file
 
 const express = require('express');
 const mongoose = require('mongoose'); 
+const routes = require('./routes/routes.js');
 const mongoString = process.env.DATABASE_URL // stores the string into a variable
 
 mongoose.connect(mongoString); // connects tha DB to my server
@@ -18,6 +19,9 @@ database.once('connected', () => {
 const app = express();
 
 app.use(express.json());
+/* This app takes two things: one is the base endpoint,
+the other is the contents of the routes. Now all my endpoints will start from /api*/
+app.use('/api', routes);
 
 app.listen(3000, () => {
   console.log(`Server started at ${3000}`)
