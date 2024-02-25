efef9320545e46cb18c4b75e8c32d3a9ed51ff43
Natalia392 <ntorrejonvalencia@gmail.com>
2024-02-25 19:56:08 -0300

    create .env and connect and run the db. log message for success or failure on conecting db

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+require('dotenv').config(); // imports content of .env file
+
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
+const mongoString = process.env.DATABASE_URL // stores the string into a variable
+
+mongoose.connect(mongoString); // connects tha DB to my server
+const database = mongoose.connection
+
+database.on('error', (error) => {
+  console.log(error);
+})
+
+database.once('connected', () => {
+  console.log('Database connected');
+})
 
 const app = express();
 
